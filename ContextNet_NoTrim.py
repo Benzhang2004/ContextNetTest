0d77f01c1ee7fb3f7409a0af18a117ef45ae4a05
Zhang Jialin <zhangjialin04@sina.com>
2022-07-20 16:37:43 +0800

    edit

--- ContextNet_NoTrim.py
+++ ContextNet_NoTrim.py
@@ -179,8 +179,8 @@ class GAN():
             noise = np.random.normal(0, 1, (batch_size,)+self.img_shape)
 
             # Train the generator (to have the discriminator label samples as valid)
-            c_loss = self.generator.fit([noise,labels], imgs, epochs=10, verbose=0)
-            g_loss = self.combined.fit([noise,labels], valid, epochs=10, verbose=0)
+            c_loss = self.generator.fit([noise,labels], imgs, epochs=5, verbose=0)
+            g_loss = self.combined.fit([noise,labels], valid, epochs=5, verbose=0)
             
             # Plot the progress
             print ("[C loss: ", c_loss.history['loss'][-1],end='] ')
