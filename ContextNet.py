5b498b674dedfc708f46ce494a7f06412103213b
Zhang Jialin <zhangjialin04@sina.com>
2022-07-24 22:08:57 +0800

    edit

--- ContextNet.py
+++ ContextNet.py
@@ -17,7 +17,7 @@ class GAN():
         self.img_gen_shape = (16,16,1)
 
         optimizer1 = adam_v2.Adam(0.00002, 0.5)
-        optimizer2 = adam_v2.Adam(0.002, 0.5)
+        optimizer2 = adam_v2.Adam(0.02)
 
         # Create Dirs
         if(not os.path.exists('models')):
