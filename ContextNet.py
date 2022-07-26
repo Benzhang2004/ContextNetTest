532b31a044115149d1c4544098c736ad0824e949
Zhang Jialin <zhangjialin04@sina.com>
2022-07-26 21:10:13 +0800

    edit

--- ContextNet.py
+++ ContextNet.py
@@ -72,41 +72,35 @@ class GAN():
 
     def build_generator(self):
 
-        model1 = Sequential()
-        model2 = Sequential()
-        model3 = Sequential()
+        model = Sequential()
 
-        model1.add(Conv2D(64,(4,4),(2,2),padding='same'))
-        model1.add(ReLU())
-        model1.add(Conv2D(128,(4,4),(2,2),padding='same'))
-        model1.add(ReLU())
-        model1.add(Conv2D(256,(4,4),(2,2),padding='same'))
-        model1.add(ReLU())
-
-        model2.add(Conv2D(512,(4,4),(2,2),padding='same'))
-        model2.add(ReLU())
-        model2.add(Flatten())
-        model2.add(Dense(16384))
-        model2.add(BatchNormalization())
-        model2.add(ReLU())
-        model2.add(Reshape((4,4,1024)))
-        model2.add(Conv2DTranspose(256,(4,4),(2,2),padding='same'))
-        model2.add(BatchNormalization())
-        model2.add(ReLU())
-
-        model3.add(Conv2DTranspose(1,(4,4),(2,2),padding='same'))
-        model3.add(BatchNormalization())
-        model3.add(ReLU())
-        model3.add(Activation('sigmoid'))
+        model.add(Conv2D(64,(4,4),(2,2),padding='same'))
+        model.add(ReLU())
+        model.add(Conv2D(128,(4,4),(2,2),padding='same'))
+        model.add(ReLU())
+        model.add(Conv2D(256,(4,4),(2,2),padding='same'))
+        model.add(ReLU())
+        model.add(Conv2D(512,(4,4),(2,2),padding='same'))
+        model.add(ReLU())
+        model.add(Flatten())
+        model.add(Dense(16384))
+        model.add(BatchNormalization())
+        model.add(ReLU())
+        model.add(Reshape((4,4,1024)))
+        model.add(Conv2DTranspose(512,(4,4),(2,2),padding='same'))
+        model.add(BatchNormalization())
+        model.add(ReLU())
+        model.add(Conv2DTranspose(1,(4,4),(2,2),padding='same'))
+        model.add(BatchNormalization())
+        model.add(ReLU())
+        model.add(Activation('sigmoid'))
 
         noise = Input(shape=self.img_shape)
         label = Input(shape=self.img_shape)
 
         model_input = merge.multiply([noise, label])
 
-        sht = model1(model_input)
-        res = merge.add([model2(sht),sht])
-        img = model3(res)
+        img = model(model_input)
         
         return Model([noise,label], img)
         
