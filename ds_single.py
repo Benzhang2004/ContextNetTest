abc867fd78833621efe7d983801f8ed8d8a7ded1
Zhang Jialin <zhangjialin04@sina.com>
2022-07-31 22:02:56 +0800

    edit SingleNet

---
+++ ds_single.py
@@ -0,0 +1,35 @@
+import ds
+import tensorflow as tf
+import numpy as np
+from PIL import Image
+import matplotlib.pyplot as plt
+
+(X_train,Y_train,y_train),(X_test,Y_test,y_test) = ds.load_data()
+y_train = np.multiply(np.where(Y_train < 0, 1, 0),X_train)
+
+fig, axs = plt.subplots(1, 4)
+axs[0].imshow(y_train[0], cmap='gray')
+axs[1].imshow(X_train[0], cmap='gray')
+axs[2].imshow(Y_train[0], cmap='gray')
+axs[0].axis('off')
+axs[1].axis('off')
+axs[2].axis('off')
+
+
+def load_data():
+    return ds.load_data()
+
+class SingleNetTrDS(tf.keras.utils.Sequence):
+    def __init__(self, batch_size):
+        self.batch_size = batch_size
+
+    def __len__(self):
+        return (ds.X_train.shape[0])/self.batch_size
+
+    def __getitem__(self, idx):
+        batch_Y = Y_train[idx * self.batch_size:(idx + 1) * self.batch_size]
+        batch_y = y_train[idx * self.batch_size:(idx + 1) * self.batch_size]
+        for i in range(idx * self.batch_size, (idx + 1) * self.batch_size):
+            ds.remask(i)
+            y_train[i] = np.multiply(np.where(Y_train[i] < 0, 1, 0),X_train[i])
+        return batch_Y, batch_y
