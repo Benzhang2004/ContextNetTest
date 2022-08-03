4a0deef98fe0c667ccbe7e1d87f9ddb8c1f27da6
Zhang Jialin <zhangjialin04@sina.com>
2022-08-03 22:32:10 +0800

    edit

--- ds_single.py
+++ ds_single.py
@@ -27,6 +27,21 @@ class SingleNetTrDS(tf.keras.utils.Sequence):
             y_train[i] = np.multiply(np.where(Y_train[i] < 0, 1, 0),X_train[i])
         return batch_Y, batch_y
 
+class SingleNetTrDS_Noshade(tf.keras.utils.Sequence):
+    def __init__(self, batch_size):
+        self.batch_size = batch_size
+
+    def __len__(self):
+        return int((ds.X_train.shape[0])/self.batch_size)
+
+    def __getitem__(self, idx):
+        batch_Y = Y_train[idx * self.batch_size:(idx + 1) * self.batch_size]
+        batch_X = X_train[idx * self.batch_size:(idx + 1) * self.batch_size]
+        for i in range(idx * self.batch_size, (idx + 1) * self.batch_size):
+            ds.remask(i)
+            Y_train[i] = ds.Y_train[i,:,:,0]
+        return batch_Y, batch_X
+
 class SingleNetPRDS(tf.keras.utils.Sequence):
     def __init__(self, batch_size, maxlen=500):
         self.batch_size = batch_size
